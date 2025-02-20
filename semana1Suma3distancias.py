48e71a85d0902857f1f7093ec71a6ee97df2bf55
linda-05 <aa0531032025@unab.edu.sv>
2025-02-20 17:19:42 -0600

    semana1 suma de 3 distancias

---
+++ semana1Suma3distancias.py
@@ -0,0 +1,20 @@
+def new_func():
+    #region ejercicio1
+# DistanciasT = D1+D2+D3+ . . . n
+# d1 = 30
+# d2 = 50
+# d3 = 40
+# Distancia total para 8 vueltas
+    d1 = 30  # Distance of segment 1
+    d2 = 50  # Distance of segment 2
+    d3 = 40  # Distance of segment 3
+    nv = 8   # Number of laps
+
+# Calculate the total distance
+    total = nv * (d1 + d2 + d3)
+
+# Print the total distance
+    print(f"distancia total {total} metros")
+    #endregion
+
+new_func()
