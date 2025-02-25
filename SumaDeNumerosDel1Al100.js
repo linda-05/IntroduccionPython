31147725b0eeeffca7a19dd3bf197fb09aa97274
linda-05 <aa0531032025@unab.edu.sv>
2025-02-25 16:00:08 -0600

    SumaDeNumerosDel1Al100.py

---
+++ SumaDeNumerosDel1Al100.js
@@ -0,0 +1,4 @@
+let num1 = parseFloat(prompt("Ingrese el primer número:"));
+let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+let suma = num1 + num2;
+console.log(`La suma de ${num1} y ${num2} es ${suma}`);
