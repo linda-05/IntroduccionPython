c5bb229225b14e8d122120fd5696da89a86c3e40
linda-05 <aa0531032025@unab.edu.sv>
2025-02-26 16:49:21 -0600

    hola.py

---
+++ hola.py
@@ -0,0 +1,11 @@
+print("Hola mundo")
+mensaje = "Bienvenidos a Python"
+print(mensaje)
+
+numero = 10
+numero2 = "10"
+numero3 = 12.645
+total = numero + int(numero2)
+print(type(total))
+print(type(numero2))
+print(type(numero3))
