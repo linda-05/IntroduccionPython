65f67fdf139d062f1d7af849d6d089bee48ee63e
linda-05 <aa0531032025@unab.edu.sv>
2025-03-02 16:21:13 -0600

    calculado el area de un triangulo

---
+++ LogicaDeProgamacionSemana2/CalcularElAreaDeUnTriangulo.py
@@ -0,0 +1,4 @@
+base = float (input("Ingrese la base del triangulo:"))
+altura = float(input("Ingrese la altura del triangulo:"))
+area = (base * altura) / 2
+print (f"El area del triangulo es {area}") 
